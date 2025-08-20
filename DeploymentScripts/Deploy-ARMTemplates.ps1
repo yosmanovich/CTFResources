e5b8be0abc9c90aac04f39aa8b5550c25d1ca1c8
William Yosmanovich <wiyosman@microsoft.com>
2025-08-20 17:31:05 -0400

    Revised script and tightened security

--- DeploymentScripts/Deploy-ARMTemplates.ps1
+++ DeploymentScripts/Deploy-ARMTemplates.ps1
@@ -27,21 +27,27 @@ if (Test-Path -Path "../Configuration/$Environment.json")
         --resource-group $($EnvironmentSettings.ResourceGroupName) `
         --template-file "../Infrastructure/Templates/KeyVault.json" `
         --parameters "../Infrastructure/Parameters/KeyVault.parameters.json" `
-        --parameters "vaultName=$($EnvironmentSettings.KeyVaultName)" `
+        --parameters "vaultName=$($EnvironmentSettings.KeyVaultName)"  `
+        --output none
+    Write-Host "Key Vault provisioned"
 
     $scope = az keyvault show --resource-group  $($EnvironmentSettings.ResourceGroupName) --name $($EnvironmentSettings.KeyVaultName) --query id -o tsv
 
     $upn = az ad signed-in-user show --query id -o tsv
     az role assignment $command --role  "Key Vault Secrets Officer" --assignee $upn --scope $scope --output none
     
-    $requiredSecrets = @("OLLAMA-ADMIN-KEY", "OLLAMA-USER-KEY")
-    $secrets = az keyvault secret list --vault-name $($EnvironmentSettings.KeyVaultName) --query "[].name" -o tsv
+    $requiredSecrets = @{
+       "OLLAMA-ADMIN-KEY" = "Not Set"
+       "OLLAMA-USER-KEY"  = "Not Set"
+    }
 
-    $requiredSecrets | ForEach-Object {    
+    $secrets = az keyvault secret list --vault-name $($EnvironmentSettings.KeyVaultName) --query "[].name" -o tsv
+    $keys = @($requiredSecrets.Keys)
+    $keys | ForEach-Object {    
         if ($secrets -notcontains $_)
         {
-            $key = Get-RandomKey
-            az keyvault secret set --vault-name $($EnvironmentSettings.KeyVaultName) --name $_ --value $key --output none
+            $requiredSecrets[$_] = Get-RandomKey
+            az keyvault secret set --vault-name $($EnvironmentSettings.KeyVaultName) --name $_ --value $requiredSecrets[$_] --output none
         }
     }
 
@@ -49,13 +55,17 @@ if (Test-Path -Path "../Configuration/$Environment.json")
         --resource-group $($EnvironmentSettings.ResourceGroupName) `
         --template-file "../Infrastructure/Templates/StorageAccount.json" `
         --parameters "../Infrastructure/Parameters/StorageAccount.parameters.json" `
-        --parameters "storageAccount_name=$($EnvironmentSettings.StorageAccountName)" `
+        --parameters "storageAccount_name=$($EnvironmentSettings.StorageAccountName)"  `
+        --output none
+    Write-Host "Storage Account provisioned"
 
     az deployment group $command --name "loganalytics" `
         --resource-group $($EnvironmentSettings.ResourceGroupName) `
         --template-file "../Infrastructure/Templates/LogAnalytics.json" `
         --parameters "../Infrastructure/Parameters/LogAnalytics.parameters.json" `
-        --parameters "analyticWorkspaceName=$($EnvironmentSettings.LogAnalyticsName)" `
+        --parameters "analyticWorkspaceName=$($EnvironmentSettings.LogAnalyticsName)"  `
+        --output none
+    Write-Host "Log Analytics provisioned"
 
     az deployment group $command --name "ContainerAppsEnvironment" `
         --resource-group $($EnvironmentSettings.ResourceGroupName) `
@@ -63,12 +73,16 @@ if (Test-Path -Path "../Configuration/$Environment.json")
         --parameters "../Infrastructure/Parameters/ContainerAppsEnvironment.parameters.json" `
         --parameters "ContainerAppsEnvironment_name=$($EnvironmentSettings.ContainerAppsEnvironmentName)" `
         --parameters "accountName=$($EnvironmentSettings.StorageAccountName)" `
-        --parameters "workspaceName=$($EnvironmentSettings.LogAnalyticsName)"
+        --parameters "workspaceName=$($EnvironmentSettings.LogAnalyticsName)" `
+        --output none
+    Write-Host "Container Apps Environment provisioned"
 
     az deployment group $command --name "ContainerRegistry" `
         --resource-group $($EnvironmentSettings.ResourceGroupName) `
         --template-file "../Infrastructure/Templates/ContainerRegistry.json" `
-        --parameters "../Infrastructure/Parameters/ContainerRegistry.parameters.json"
+        --parameters "../Infrastructure/Parameters/ContainerRegistry.parameters.json" `
+        --output none
+    Write-Host "Container Registry provisioned"
 
     $scope = az acr show --resource-group  $($EnvironmentSettings.ResourceGroupName) --name $($EnvironmentSettings.ContainerRegistryName) --query id -o tsv
     $upn = az containerapp env identity show --name $($EnvironmentSettings.ContainerAppsEnvironmentName) --resource-group $($EnvironmentSettings.ResourceGroupName) --query principalId -o tsv
@@ -83,7 +97,9 @@ if (Test-Path -Path "../Configuration/$Environment.json")
         --parameters "../Infrastructure/Parameters/ContainerApp.$($EnvironmentSettings.ContainerAppLLM).parameters.json" `
         --parameters "containerapp_name=$($EnvironmentSettings.ContainerAppLLM)" `
         --parameters "managedenvironment_name=$($EnvironmentSettings.ContainerAppsEnvironmentName)" `
-        --parameters "registry=$ContainerRegistryAddress"
+        --parameters "registry=$ContainerRegistryAddress" `
+        --output none
+    Write-Host "Ollama container app provisioned"
 
     az deployment group $command --name "ContainerApp" `
         --resource-group $($EnvironmentSettings.ResourceGroupName) `
@@ -91,7 +107,9 @@ if (Test-Path -Path "../Configuration/$Environment.json")
         --parameters "../Infrastructure/Parameters/ContainerApp.$($EnvironmentSettings.ContainerAppGUI).parameters.json" `
         --parameters "containerapp_name=$($EnvironmentSettings.ContainerAppGUI)" `
         --parameters "managedenvironment_name=$($EnvironmentSettings.ContainerAppsEnvironmentName)" `
-        --parameters "registry=$ContainerRegistryAddress"
+        --parameters "registry=$ContainerRegistryAddress" `
+        --output none
+    Write-Host "Ollama GUI container app provisioned"
 
     az deployment group $command --name $($EnvironmentSettings.ContainerChainlit)`
         --resource-group $($EnvironmentSettings.ResourceGroupName) `
@@ -99,7 +117,9 @@ if (Test-Path -Path "../Configuration/$Environment.json")
         --parameters "../Infrastructure/Parameters/ContainerApp.$($EnvironmentSettings.ContainerChainlit).parameters.json" `
         --parameters "containerapp_name=$($EnvironmentSettings.ContainerChainlit)" `
         --parameters "managedenvironment_name=$($EnvironmentSettings.ContainerAppsEnvironmentName)" `
-        --parameters "registry=$ContainerRegistryAddress"        
+        --parameters "registry=$ContainerRegistryAddress" `
+        --output none
+    Write-Host "Chainlit container app provisioned"
 
     az deployment group $command --name $($EnvironmentSettings.ContainerAppProxy)`
         --resource-group $($EnvironmentSettings.ResourceGroupName) `
@@ -107,7 +127,8 @@ if (Test-Path -Path "../Configuration/$Environment.json")
         --parameters "../Infrastructure/Parameters/ContainerApp.$($EnvironmentSettings.ContainerAppProxy).parameters.json" `
         --parameters "containerapp_name=$($EnvironmentSettings.ContainerAppProxy)" `
         --parameters "managedenvironment_name=$($EnvironmentSettings.ContainerAppsEnvironmentName)" `
-        --parameters "registry=$ContainerRegistryAddress"
+        --parameters "registry=$ContainerRegistryAddress" `
+        --output none
 
     $scope = az keyvault show --resource-group  $($EnvironmentSettings.ResourceGroupName) --name $($EnvironmentSettings.KeyVaultName) --query id -o tsv
     $upn = az containerapp identity show --name $($EnvironmentSettings.ContainerAppProxy) --resource-group $($EnvironmentSettings.ResourceGroupName) --query principalId -o tsv
@@ -121,9 +142,12 @@ if (Test-Path -Path "../Configuration/$Environment.json")
          --resource-group $($EnvironmentSettings.ResourceGroupName) `
          --secrets "admin-key=keyvaultref:https://$($EnvironmentSettings.KeyVaultName).vault.azure.net/secrets/OLLAMA-USER-KEY,identityref:system" --output none        
 
+    Write-Host "Ollama proxy container app provisioned"
+
     az containerapp exec --name $($EnvironmentSettings.ContainerAppLLM) `
         --resource-group $($EnvironmentSettings.ResourceGroupName) `
         --command "ollama pull tinyllama:latest"  --output none
+    Write-Host "Ollama pulled tinyllama:latest"
 
     $revision = az containerapp revision list `
          --name $($EnvironmentSettings.ContainerAppProxy) `
@@ -131,7 +155,13 @@ if (Test-Path -Path "../Configuration/$Environment.json")
 
     az containerapp revision restart `
         --name $($EnvironmentSettings.ContainerAppProxy) `
-        --resource-group $($EnvironmentSettings.ResourceGroupName) --revision  $revision      
+        --resource-group $($EnvironmentSettings.ResourceGroupName) --revision  $revision  `
+        --output none
+    
+    Write-Host "Ollama Proxy API Keys:"
+    $requiredSecrets | ForEach-Object {
+        Write-Host "$_ : $requiredSecrets[$_]"
+    }
 }
 else
 {
